8c7a1191fb434fb996d3bb136dd218901aa22017
Agustin Ottonello <agustinottonello@gmail.com>
2024-04-03 23:28:18 -0600

    segundo commit

--- main.go
+++ main.go
@@ -5,4 +5,7 @@ import "github.com/Agusotto1/goDesde0/variables"
  func main(){
 	 fmt.Println("Hola Mundo")
 	 variables.MuestroEnteros()
- } 
+	 //variables.RestoVariables()
+	estado, texto := variables.ConvertirTexto(1500)
+	fmt.Println(texto, estado)
+ }
---
+++ variables/resto.go
@@ -0,0 +1,29 @@
+package variables
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+
+func RestoVariables() {
+	Nombre = "Pedro"
+	Estado = true
+	Sueldo = 1500.20
+	Fecha = time.Now()
+
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)
+}
+func ConvertirTexto(numero int) (bool, string) {
+	var texto = strconv.Itoa(numero)
+
+	return true, texto
+}
