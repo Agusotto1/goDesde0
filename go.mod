9123c90c9e2f0395010a2145e474f155928e2e08
Agustin Ottonello <agustinottonello@gmail.com>
2024-04-03 22:34:45 -0600

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Agusotto1/goDesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "github.com/Agusotto1/goDesde0/variables"
+ func main(){
+	 fmt.Println("Hola Mundo")
+	 variables.MuestroEnteros()
+ } 
